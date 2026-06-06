47c0dc4f64129694fa944ff2211ac2ac2023760a
Jade Chi-yen <chiyenjade@gmail.com>
2026-06-07 01:58:30 +0200

    add: errors

---
+++ errors.go
@@ -0,0 +1,14 @@
+package main
+
+import "errors"
+
+var (
+	ErrNotFound            = errors.New("ressource introuvable")
+	ErrInsufficientCredits = errors.New("crédits insuffisants")
+	ErrExchangeConflict    = errors.New("un échange est déjà en cours pour ce service")
+	ErrSelfExchange        = errors.New("impossible de s'échanger un service à soi-même")
+	ErrExchangeNotDone     = errors.New("l'échange n'est pas terminé")
+	ErrAlreadyReviewed     = errors.New("vous avez déjà laissé un avis pour cet échange")
+	ErrInvalidStatus       = errors.New("transition de statut invalide")
+	ErrUnauthorized        = errors.New("action non autorisée")
+)
